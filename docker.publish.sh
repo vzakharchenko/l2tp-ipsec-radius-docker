2ec3949fdb70ecc588d2e159707ad1f322be3bed
vzakharchenko <vzakharchenko@modeln.com>
2021-01-10 16:21:11 +0200

    initial Commit

--- docker.publish.sh
+++ docker.publish.sh
@@ -1,6 +1,6 @@
 docker build -t l2tp-ipsec-radius-docker .
 docker tag  l2tp-ipsec-radius-docker vassio/l2tp-ipsec-radius-docker:1.0.0
-docker push vassio/l2tp-ipsec-radius-docker:1.0.
+docker push vassio/l2tp-ipsec-radius-docker:1.0.0
 
 docker tag  l2tp-ipsec-radius-docker vassio/l2tp-ipsec-radius-docker:latest
 docker push vassio/l2tp-ipsec-radius-docker:latest
