59cec3a9cfa7068870c13788a257a4424e217bb4
vzakharchenko <vzakharchenko@modeln.com>
2021-01-10 16:12:29 +0200

    initial Commit

--- l2tp-js/generateDockerCommands.js
+++ l2tp-js/generateDockerCommands.js
@@ -3,39 +3,39 @@ const fs = require('fs');
 const configPath = process.env['CONFIG_PATH'] || '../examples/config.json';
 
 function parseFile(cJson) {
-    const ips = {};
-    const ports = {};
     let redir = '';
-    Object.entries(cJson).forEach((entry) => {
-        const user = entry[0];
-        const value = entry[1];
-        if (!value.ip) {
-            throw new Error(user + " does not have RemoteIp address")
+    let radsec = '';
+    if (cJson.radsec) {
+        if (!fs.existsSync(cJson.radsec.privateKey)) {
+            throw new Error('Radsec Private key does not exist')
         }
-        if (!value.password) {
-            throw new Error(user + " does not have password")
+        if (!fs.existsSync(cJson.radsec.certificateFile)) {
+            throw new Error('Radsec Public key does not exist')
         }
-        if (ips[value.ip]) {
-            throw new Error(value.ip + " already exists. user " + ips[value.ip].user)
+        if (!fs.existsSync(cJson.radsec.CACertificateFile)) {
+            throw new Error('Radsec CA key does not exist')
         }
-        ips[value.ip] = {user};
-        if (value.forwarding) {
-            value.forwarding.forEach((f) => {
-                if (ports[f.destinationPort]) {
-                    throw new Error(f.destinationPort + " already exists. user " + ports[f.destinationPort].user)
-                }
-                ports[f.destinationPort] = {user};
-                redir = redir + `-p ${f.destinationPort}:${f.destinationPort} `
-            })
-        }
-    });
+        radsec = `-v ${cJson.radsec.privateKey}:${cJson.radsec.privateKey} -v ${cJson.radsec.certificateFile}:${cJson.radsec.certificateFile} -v ${cJson.radsec.CACertificateFile}:${cJson.radsec.CACertificateFile} `
+    }
+    if (cJson.authorizationMap) {
+        if (cJson.authorizationMap.roles) {
+            Object.entries(cJson.authorizationMap.roles).forEach(((kv) => {
+                const value = kv[1];
+                if (value.forwarding) {
+                    value.forwarding.forEach(f => {
+                        redir = redir + `-p ${f.externalPort}:${f.externalPort} `
+                    })
 
-    console.log('docker run -d --name=l2tp-ipsec-radius-docker -p 500:500/udp -p 4500:4500/udp  ' + redir +
-        `-v ${configPath}:/opt/config.json --privileged --restart=always vassio/l2tp-ipsec-radius-docker:latest`)
+                }
+            }));
+        }
+    }
+    console.log('docker run -d --name=l2tp-ipsec-radius-docker -p 500:500/udp -p 4500:4500/udp -p 3799:3799 ' + redir +
+        `-v ${configPath}:/opt/config.json ${radsec} --privileged --restart=always vassio/l2tp-ipsec-radius-docker:latest`)
 }
 
 const f = fs.readFileSync(configPath, 'utf8');
 
 const configJson = JSON.parse(f);
-parseFile(configJson.users);
+parseFile(configJson);
 
