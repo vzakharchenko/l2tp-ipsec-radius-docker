563f1fe2fbd4a932b49c68afd486629ef22461e4
vzakharchenko <vzakharchenko@modeln.com>
2021-01-16 22:56:30 +0200

    support arm64

--- docker.publish.arm64.sh
+++ docker.publish.arm64.sh
@@ -1,6 +1,6 @@
 docker build -t l2tp-ipsec-radius-docker .
-docker tag  l2tp-ipsec-radius-docker vassio/l2tp-ipsec-radius-docker:1.3.2@arm64
-docker push vassio/l2tp-ipsec-radius-docker:1.3.2@arm64
+docker tag  l2tp-ipsec-radius-docker vassio/l2tp-ipsec-radius-docker:1.3.2_arm64
+docker push vassio/l2tp-ipsec-radius-docker:1.3.2_arm64
 
-docker tag  l2tp-ipsec-radius-docker vassio/l2tp-ipsec-radius-docker:latest@arm64
-docker push vassio/l2tp-ipsec-radius-docker:latest@arm64
+docker tag  l2tp-ipsec-radius-docker vassio/l2tp-ipsec-radius-docker:latest_arm64
+docker push vassio/l2tp-ipsec-radius-docker:latest_arm64
